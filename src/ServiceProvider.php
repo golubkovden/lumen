469be8c93c8b23ac0fad8e184bbbdda8a5f0794e
dgolubkov <golubkovden@gmail.com>
2017-03-31 20:50:36 +0300

    loading config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace FondBot\Frameworks\Lumen;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Laravel\Lumen\Application;
 
+/**
+ * Class ServiceProvider
+ * @property Application $app
+ * @package FondBot\Frameworks\Lumen
+ */
 class ServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
+//        $this->app->register(Providers\ConfigServiceProvider::class);
         $this->app->register(Providers\RouteServiceProvider::class);
     }
 }
